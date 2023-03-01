57d6ac47f58aa394136b02b94e379d64f0e25d93
Matt3010 <scanferla935@gmail.com>
2023-03-01 16:05:28 +0100

    intro-section

---
+++ script.js
@@ -0,0 +1,24 @@
+function MenuON(){
+
+
+let menu = document.getElementById("menu_mobile")
+menu.style.display= ""
+
+let gray = document.getElementById("ppp")
+gray.style.display= ""
+
+
+}
+
+function MenuOFF(){
+
+
+    let menu = document.getElementById("menu_mobile")
+    menu.style.display= "none"
+
+    let gray = document.getElementById("ppp")
+    gray.style.display= "none"
+    
+    
+    
+    }
